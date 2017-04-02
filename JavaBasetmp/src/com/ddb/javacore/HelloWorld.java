39ebb929e39704b8121c06b0f73ce0407065d4da
mxk <313989006@qq.com>
2017-04-02 17:10:11 +0800

    4.2hhahoooe

---
+++ JavaBasetmp/src/com/ddb/javacore/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.ddb.javacore;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// System.out.println("HelloWorld!");
+
+		System.out.println("我的姓名是 : ");
+		System.out.println("我的年龄是 : ");
+		System.out.println("我的爱好是 : ");
+	}
+
+}
